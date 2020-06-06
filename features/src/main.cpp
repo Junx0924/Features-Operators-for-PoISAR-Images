1ede5bc67601d13d9af14bc5496d61f1a30eb265
Junx0924 <glamorbree@gmail.com>
2020-06-06 02:47:20 +0200

    examples of using Oberpfaffenhofen to get cv features

--- features/src/main.cpp
+++ features/src/main.cpp
@@ -13,7 +13,7 @@ int main() {
     string ratfolder = "E:\\Oberpfaffenhofen\\sar-data";
     string labelfolder = "E:\\Oberpfaffenhofen\\label";
 
-    // set patch size 64, maximum sample points per class is 2000
+    // set patch size 64, maximum sample points per class is 2000d
     polsar* ober = new polsar(ratfolder, labelfolder,64,2000); 
      
 
@@ -26,6 +26,19 @@ int main() {
     vector<unsigned char> MPLabels;
     f.GetMP(MPfeatures, MPLabels);
 
+    vector<Mat> MPfeatures;
+    vector<unsigned char> MPLabels;
+    f.GetMP(MPfeatures, MPLabels);
     
+    vector<Mat> texture;
+    vector<unsigned char> textureLabels;
+    f.GetLBP(texture, textureLabels, 1, 8, 32);
+    f.GetGLCM(texture, textureLabels, 5, GrayLevel::GRAY_8, 32);
+
+    vector<Mat> colorfeatures;
+    vector<unsigned char> colorLabels;
+    f.GetMPEG7DCD(colorfeatures, colorLabels, 3);
+    f.GetMPEG7CSD(colorfeatures, colorLabels, 32);
+
     return 0; // success
 }
