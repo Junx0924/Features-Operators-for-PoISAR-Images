a4b5a650297cb19dfd8a780819b52b8629d14c37
Junx0924 <glamorbree@gmail.com>
2020-06-26 20:36:24 +0200

    correct some errors of getting mat data pointer

--- features/src/mp.cpp
+++ features/src/mp.cpp
@@ -3,16 +3,24 @@
 using namespace cv;
 
 auto* mp::matToArray(const Mat& image) {
+    Mat output;
     if (image.channels() != 1) {
-        cvtColor(image, image, COLOR_BGR2GRAY);
+        cvtColor(image, output, COLOR_BGR2GRAY);
     }
+    else {
+        output = image.clone();
+    }
+   int totalElements = static_cast<int>(output.total()) * output.channels();
+    if (!output.isContinuous()) {
+        output = output.clone();
+    }
+
     // flatten the Mat.
-    uint totalElements = static_cast<int>(image.total()) * image.channels();
-    Mat flat = image.reshape(1, totalElements);
-    if (!image.isContinuous()) {
-        flat = flat.clone();
+    Mat flat = output.reshape(1, totalElements);
+    unsigned char* ptr = new uchar[totalElements];
+    for (int i = 0; i < totalElements; i++) {
+        ptr[i] = flat.at<uchar>(i, 0);
     }
-    auto* ptr = flat.data;
     return ptr;
 }
 
@@ -31,14 +39,15 @@ Mat mp::CaculateMP(const Mat& src, int morph_size) {
     //imshow("image", dst);
     // waitKey(0);
 
-    Mat element = getStructuringElement(MORPH_ELLIPSE, cv::Size(2 * morph_size + 1, 2 * morph_size + 1), cv::Point(morph_size, morph_size));
-
+   // Mat element = getStructuringElement(MORPH_ELLIPSE, cv::Size(2 * morph_size + 1, 2 * morph_size + 1), cv::Point(morph_size, morph_size));
+    Mat element = getStructuringElement(MORPH_RECT, cv::Size(1, morph_size));
 
     //erode and reconstruct ( opening-by-reconstruction )
     Mat Iobr = Mat(Size(dst.size()), dst.type());
     erode(dst, Iobr, element);
     auto* ptr = matToArray(Iobr);
     mp::Reconstruct(ptr, matToArray(dst), dst.cols, dst.rows);
+   
     //restore cv Mat
     Mat dst2 = Mat(dst.rows, dst.cols, dst.type(), ptr);
    // imshow("openning by reconstruction: ", dst2);
@@ -47,16 +56,20 @@ Mat mp::CaculateMP(const Mat& src, int morph_size) {
      //dilate and reconstruct (closing-by-Reconstruction)
     Mat Icbr = Mat(Size(dst2.size()), dst2.type());
     dilate(dst2, Icbr, element);
+   
     // imcomplement
     dst2 = 255 - dst2;
     Icbr = 255 - Icbr;
     auto* ptr2 = matToArray(Icbr);
     mp::Reconstruct(ptr2, matToArray(dst2), dst2.cols, dst2.rows);
+  
     //restore cv Mat
     Mat dst3 = Mat(dst.rows, dst.cols, dst.type(), ptr2);
+    
     // imcomplement
     dst3 = 255 - dst3;
    // imshow("opening-closing by reconstruction: ", dst3);
-    //waitKey(0);
-   return dst3;
+    // waitKey(0);
+    
+    return dst3;
 }
