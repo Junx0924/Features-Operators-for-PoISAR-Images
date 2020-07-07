8cb7dff5fa1d9906ca9d6fc6966f95d6fae56fd8
Junx0924 <glamorbree@gmail.com>
2020-07-07 22:30:52 +0200

    update

--- features/src/main.cpp
+++ features/src/main.cpp
@@ -6,17 +6,17 @@
 using namespace std;
 using namespace cv;
 
-string MP = "/MP";
+string CTelements = "/CTelements";
 string decomp = "/decomp";
+string MP = "/MP";
 string color = "/color";
 string texture = "/texture";
 string polStatistic = "/polStatistic";
-string CTelements = "/CTelements";
 vector<string> dataset_name = { "/feature" ,"/patchLabel" };
  
 int main(int argc, char** argv) {
 
-    if (argc < 5) {
+    if (argc < 6) {
         cout << "Usage: " << argv[0] << " <ratFolder> <labelFolder> <oberFile> <featureName>  <filterSize> \n" << endl;
         cout << "e.g. " << argv[0] << " E:\\Oberpfaffenhofen\\sar-data E:\\Oberpfaffenhofen\\label E:\\MP.h5 /MP 0\n" << endl;
         cout << "featureName choose from: \n" << MP << "," << decomp << "," << color << "," << texture << "," << polStatistic << "," << CTelements << endl;
@@ -24,14 +24,15 @@ int main(int argc, char** argv) {
         return 0;
     }
 
-    string ratfolder = argv[1]; // "E:\\Oberpfaffenhofen\\sar-data";
-    string labelfolder = argv[2]; //"E:\\Oberpfaffenhofen\\label";
-    string oberfile = argv[3]; //"E:\\MP.h5";
-    string feature_name = argv[4]; //MP;
+    string ratfolder = argv[1];  
+    string labelfolder = argv[2]; 
+    string oberfile = argv[3];  
+    string feature_name = argv[4];  
     int filterSize = stoi(argv[5]);  
     if ((filterSize != 5) && (filterSize != 7) && (filterSize != 9) && (filterSize != 11)) { filterSize = 0;}
-    unsigned char classlabel = 255; //all the class
-    int numOfSamplePoints = 0; //all the points
+
+    unsigned char classlabel = 255; 
+    int numOfSamplePoints = 0; 
     int batchSize = 5000;
     int patchSize = 10;
     if (feature_name == CTelements) { patchSize = 3; }
@@ -52,6 +53,7 @@ int main(int argc, char** argv) {
     Utils::classifyFeaturesML(oberfile, feature_name, "opencvFLANN", 80, filterSize, patchSize, batchSize);
     
     Utils::generateColorMap(oberfile, feature_name, "opencvFLANN", filterSize, patchSize, batchSize);
+
     Utils::featureDimReduction(oberfile, feature_name, 3000,filterSize, patchSize);
 
     return 0;
@@ -68,7 +70,7 @@ int main(int argc, char** argv) {
 //    // test features
 //    string feature_name = CTelements;
 //    unsigned char classlabel = 255;
-//    int numOfSamplePoints = 3000;
+//    int numOfSamplePoints = 0;
 //    int patchSize = 3;
 //    int filterSize = 0;
 //    int batchSize = 5000;
