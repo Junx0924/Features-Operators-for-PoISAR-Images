da5be4471c12a592cd144142e861db08dbb62304
Anupama <anupamar228@gmail.com>
2020-08-09 16:29:08 +0200

    Segregrated features, all commits done

--- Modern_Features/Visualisation.cpp
+++ Modern_Features/Visualisation.cpp
@@ -1,25 +1,22 @@
 #include "Visualisation.h"
 
 /***********************************************************************
-Calculate the mean of each feature vector in the feature map
+Perform contrast correction on each feature vector in the feature map
 Author : Anupama Rajkumar
-Date : 21.07.2020
-Description: This function calculates the mean of each feature point if the
-size of the feature vector > 1
+Description: This functions converts vector to matrix and performs
+contrast correction by applying gray scale stretching and histogram 
+equalisation
 Input 1 : The calculated feature vector
-Input 2 : The calculated mean output which is also linearly stretched for
-contrast correction
+Input 2 : The count number of the current feature vector
+Output	: Contast corrected matrix
 *************************************************************************/
-void Visual::CalculateMeanFeatureVector(vector<vector<float>>& featureVector, Mat& outPut) {
+void Visual::ContrastCorrection(vector<vector<float>>& featureVector, int cnt, Mat& outPut) {
 
 		vector<float> outVec, outVecCpy;
-		for (int cnt = 0; cnt < featureVector.size(); cnt++) {
-			float mean = 0.;
-			for (int s = 0; s < featureVector[cnt].size(); s++) {
-				mean += featureVector[cnt][s];
-				mean /= featureVector[cnt].size();
-			}
-			outVec.push_back(mean);
+		outVec.clear();
+		outVecCpy.clear();
+		for (int s = 0; s < featureVector.size(); s++) {
+			outVec.push_back(featureVector[s][cnt]);
 		}
 		outVecCpy = outVec;
 		float min = 0., max = 0.;
@@ -35,7 +32,6 @@ void Visual::CalculateMeanFeatureVector(vector<vector<float>>& featureVector, Ma
 			memcpy(OutMat.data, outVec.data(), outVec.size() * sizeof(float));
 		}
 
-
 		/*scale values of outMat to outPut*/
 		for (int row = 0; row < OutMat.rows; row++) {
 			for (int col = 0; col < OutMat.cols; col++) {
@@ -46,41 +42,37 @@ void Visual::CalculateMeanFeatureVector(vector<vector<float>>& featureVector, Ma
 		}
 }
 
+
 /***********************************************************************
 Generating the feature map
 Author : Anupama Rajkumar
-Date : 21.07.2020
 Description: This function generates the visualisation of the calculated
-feature map
-Input : The calculated feature vector
+feature map after contrast correction
+Input 1 : The calculated feature vector
+Input 2 : Generated colormap image name
 *************************************************************************/
 
-
-void Visual::GenerateFeatureMap(vector<vector<float>>& m_featureVector) {
+void Visual::GenerateFeatureMap(vector<vector<float>>& m_featureVector, string& imagePrefix) {
 	/*Visualising the feature map*/
 	cout << "Visualising the feature vector...." << endl;
-	Mat meanMat;
-	int row = 6640, col = 1390;
-
-	meanMat = Mat(row, col, CV_32FC1);
-	this->CalculateMeanFeatureVector(m_featureVector, meanMat);
-	/*Generate colormap*/
-	cout << "Generating colormap" << endl;
-	Mat outMat;
-	meanMat.convertTo(meanMat, CV_8UC1);
-	outMat = meanMat.clone();
-	/*apply color map*/
-#if 0
-	cv::Mat mlookUpTable_8UC1(1, 256, CV_8UC1);
-	for (int i = 0; i < 256; ++i)
-	{
-		mlookUpTable_8UC1.at<uchar>(0, i) = uchar(255 - i);
+	
+	int row = 6640, col = 1390;		
+	int size = m_featureVector[0].size();
+	for (int cnt = 0; cnt < size; cnt++) {
+		cout << "Feature map " << cnt + 1 << endl;
+		Mat featureMat;
+		featureMat = Mat(row, col, CV_32FC1);
+		this->ContrastCorrection(m_featureVector, cnt, featureMat);
+		/*Generate colormap*/
+		cout << "Generating colormap" << endl;
+		Mat outMat;
+		featureMat.convertTo(featureMat, CV_8UC1);
+		outMat = featureMat.clone();
+		/*equalising histogram for better contrast*/
+		cv::equalizeHist(outMat, outMat);
+		cv::applyColorMap(outMat, outMat, COLORMAP_JET);
+		string filename = imagePrefix + to_string(cnt) + ".png";
+		cv::imwrite(filename, outMat);
 	}
-	cv::LUT(meanMat, mlookUpTable_8UC1, outMat);
-#endif
-	/*equalising histogram for better contrast*/
-	cv::equalizeHist(outMat, outMat);
-	applyColorMap(outMat, outMat, COLORMAP_JET);
-	cv::imwrite("PatchColorMap_color.png", outMat);
 
 }
